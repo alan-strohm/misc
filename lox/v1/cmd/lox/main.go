1876038f4cf0aa888de2c580ee751312d11de4f0
Alan Strohm <3605394+alan-strohm@users.noreply.github.com>
2021-08-01 06:12:55 -0700

    lox 4.1

---
+++ lox/v1/cmd/lox/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func run(code string) error {
+	return nil
+}
+
+func runFile(fname string) error {
+	f, err := os.Open(fname)
+	if err != nil {
+		return err
+	}
+	b, err := io.ReadAll(f)
+	if err != nil {
+		return err
+	}
+	return run(string(b))
+}
+
+func runPrompt() error {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("> ")
+		if !scanner.Scan() {
+			break
+		}
+		if err := run(scanner.Text()); err != nil {
+			return err
+		}
+	}
+	return scanner.Err()
+}
+
+func main() {
+	var err error
+	if len(os.Args) > 2 {
+		fmt.Println("Usage: lox [script]")
+		os.Exit(64)
+	} else if len(os.Args) == 2 {
+		err = runFile(os.Args[1])
+	} else {
+		err = runPrompt()
+	}
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
+}
