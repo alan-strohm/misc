812ab4d7b4cbb1b4a731e368bbd8ddd36217824d
Alan Strohm <3605394+alan-strohm@users.noreply.github.com>
2021-12-02 03:22:16 -0800

    try out an approach with interfaces

--- adventofcode2021/day02/day02.go
+++ adventofcode2021/day02/day02.go
@@ -7,51 +7,53 @@ import (
 	"strings"
 )
 
-type sub struct {
-	depth, pos, aim int
-	part1           bool
+type sub interface {
+	forward(units int)
+	down(units int)
+	depth() int
+	pos() int
 }
 
-func (s *sub) do(cmd string, arg int) {
-	if s.part1 {
-		s.p1(cmd, arg)
-	} else {
-		s.p2(cmd, arg)
-	}
+type p1 struct {
+	d, p int
 }
 
-func (s *sub) p1(cmd string, arg int) {
-	switch cmd {
-	case "forward":
-		s.pos += arg
-	case "down":
-		s.depth += arg
-	case "up":
-		s.depth -= arg
-	}
+func (p *p1) forward(units int) { p.p += units }
+func (p *p1) down(units int)    { p.d += units }
+func (p *p1) depth() int        { return p.d }
+func (p *p1) pos() int          { return p.p }
+
+type p2 struct {
+	d, p, aim int
 }
 
-func (s *sub) p2(cmd string, arg int) {
-	switch cmd {
-	case "forward":
-		s.pos += arg
-		s.depth += arg * s.aim
-	case "down":
-		s.aim += arg
-	case "up":
-		s.aim -= arg
-	}
+func (p *p2) forward(units int) {
+	p.p += units
+	p.d += units * p.aim
 }
+func (p *p2) down(units int) { p.aim += units }
+func (p *p2) depth() int     { return p.d }
+func (p *p2) pos() int       { return p.p }
 
 func Run(scanner *bufio.Scanner, part1 bool) (int, error) {
-	s := sub{part1: part1}
+	var s sub = &p1{}
+	if !part1 {
+		s = &p2{}
+	}
 	for i := 0; scanner.Scan(); i++ {
 		parts := strings.Split(scanner.Text(), " ")
 		arg, err := strconv.Atoi(parts[1])
 		if err != nil {
 			return 0, fmt.Errorf("invalid line [%d]: %s", i, scanner.Text())
 		}
-		s.do(parts[0], arg)
+		switch parts[0] {
+		case "forward":
+			s.forward(arg)
+		case "down":
+			s.down(arg)
+		case "up":
+			s.down(-arg)
+		}
 	}
-	return s.pos * s.depth, nil
+	return s.pos() * s.depth(), nil
 }
