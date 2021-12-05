8585c3145fb475363bff6b43717ae01f11de1b5b
Alan Strohm <3605394+alan-strohm@users.noreply.github.com>
2021-12-05 00:21:30 -0800

    adventofcode2021: day05 part2 - even simpler

--- adventofcode2021/day05/day05.go
+++ adventofcode2021/day05/day05.go
@@ -7,23 +7,18 @@ import (
 )
 
 type point struct {
-	x, y float64
+	x, y int
 }
 
-func (p *point) String() string      { return fmt.Sprintf("%f,%f", p.x, p.y) }
-func (p *point) round() *point       { return &point{math.RoundToEven(p.x), math.RoundToEven(p.y)} }
+func (p *point) String() string      { return fmt.Sprintf("%d,%d", p.x, p.y) }
 func (p *point) add(o *point) *point { return &point{p.x + o.x, p.y + o.y} }
+func (p *point) eq(o *point) bool    { return p.x == o.x && p.y == o.y }
 
-func (p *point) eq(o *point) bool {
-	pr, or := p.round(), o.round()
-	return pr.x == or.x && pr.y == or.y
-}
-
-// Return a unit vector pointing from p to o.
+// Return a "unit" vector pointing from p to o.
 func (p *point) vecTo(o *point) *point {
-	dx, dy := o.x-p.x, o.y-p.y
+	dx, dy := float64(o.x-p.x), float64(o.y-p.y)
 	d := math.Sqrt(dx*dx + dy*dy)
-	return &point{dx / d, dy / d}
+	return &point{int(math.Round(dx / d)), int(math.Round(dy / d))}
 }
 
 type line struct {
@@ -35,7 +30,7 @@ func (l *line) diag() bool { return l.start.x != l.end.x && l.start.y != l.end.y
 func (l *line) points() []point {
 	v := l.start.vecTo(&l.end)
 	r := make([]point, 0, 2)
-	for i := &l.start; !l.end.eq(i); i = i.add(v).round() {
+	for i := &l.start; !l.end.eq(i); i = i.add(v) {
 		r = append(r, *i)
 	}
 	return append(r, l.end)
@@ -43,7 +38,7 @@ func (l *line) points() []point {
 
 func parseLine(s string) (*line, error) {
 	r := &line{}
-	if _, err := fmt.Sscanf(s, "%f,%f -> %f,%f\n", &r.start.x, &r.start.y, &r.end.x, &r.end.y); err != nil {
+	if _, err := fmt.Sscanf(s, "%d,%d -> %d,%d\n", &r.start.x, &r.start.y, &r.end.x, &r.end.y); err != nil {
 		return nil, err
 	}
 	return r, nil
