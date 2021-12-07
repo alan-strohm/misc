9093190333fdaf7bde808635c93e5bb2b92285d8
Alan Strohm <3605394+alan-strohm@users.noreply.github.com>
2021-12-07 07:17:28 -0800

    adventofcode2021: day07 part2

--- adventofcode2021/day07/day07.go
+++ adventofcode2021/day07/day07.go
@@ -9,11 +9,34 @@ import (
 )
 
 func sum(in []float64, x float64) float64 {
-	r := float64(0)
+	r := 0.0
 	for _, i := range in {
 		r += math.Abs(x - i)
 	}
 	return r
+}
+
+func p2sum(in []float64, x float64) float64 {
+	r := 0.0
+	for _, i := range in {
+		d := math.Abs(x - i)
+		r += d * (d + 1) / 2
+	}
+	return r
+}
+
+func solve(in []float64) float64 {
+	i := len(in) / 2
+	best := p2sum(in, in[i])
+	for _, inc := range []float64{1, -1} {
+		j := in[i] + inc
+		for next := p2sum(in, j); next < best; {
+			best = next
+			j += inc
+			next = p2sum(in, j)
+		}
+	}
+	return best
 }
 
 func Run(scanner *bufio.Scanner, p1 bool) (int, error) {
@@ -28,5 +51,8 @@ func Run(scanner *bufio.Scanner, p1 bool) (int, error) {
 		pos[i] = float64(n)
 	}
 	sort.Float64s(pos)
-	return int(sum(pos, pos[len(pos)/2])), nil
+	if p1 {
+		return int(sum(pos, pos[len(pos)/2])), nil
+	}
+	return int(solve(pos)), nil
 }
--- adventofcode2021/day07/day07_test.go
+++ adventofcode2021/day07/day07_test.go
@@ -10,6 +10,8 @@ func TestPart1(t *testing.T) {
 	testCases := []*lib.TestCase{
 		{FName: "./sample.txt", Part1: true, Want: 37},
 		{FName: "./input.txt", Part1: true, Want: 355521},
+		{FName: "./sample.txt", Part1: false, Want: 168},
+		{FName: "./input.txt", Part1: false, Want: 100148777},
 	}
 	lib.Test(t, testCases, Run)
 }
