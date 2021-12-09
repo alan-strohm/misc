c77d1bc40a2145b988ac2a81c2794b78f8e25296
Alan Strohm <3605394+alan-strohm@users.noreply.github.com>
2021-12-09 00:33:13 -0800

    adventofcode2021: day08 - part2

--- adventofcode2021/day08/day08.go
+++ adventofcode2021/day08/day08.go
@@ -3,80 +3,18 @@ package day08
 import (
 	"bufio"
 	"fmt"
+	"math"
+	"sort"
+	"strings"
 
 	"github.com/alan-strohm/misc/adventofcode2021/lib"
 )
 
-type pat []byte
-
-type entry struct {
-	signals  [10]pat
-	outputs  [4]pat
-	sigOpts  map[string][]int
-	outOpts  map[string][]int
-	sigToOut map[string]string
-}
-
-var lenOpts = map[int][]int{
-	2: []int{1},
-	3: []int{7},
-	4: []int{4},
-	5: []int{2, 3, 5},
-	6: []int{0, 6, 9},
-	7: []int{8},
-}
-
-func (e *entry) Scan(s fmt.ScanState, verb rune) error {
-	e.sigOpts = make(map[string][]int)
-	e.outOpts = make(map[string][]int)
-	lib.Dbg(fmt.Sprintf("new entry\n"))
-	for i := 0; i < len(e.signals)+len(e.outputs)+1; i++ {
-		t, err := s.Token(true, nil)
-		if err != nil {
-			return err
-		}
-		iOutput := i - len(e.signals) - 1
-		switch {
-		case i < len(e.signals):
-			e.signals[i] = t
-			opts, ok := lenOpts[len(t)]
-			if !ok {
-				return fmt.Errorf("unexpected length for pattern: '%s'", string(t))
-			}
-			e.sigOpts[string(t)] = opts
-		case i == len(e.signals):
-			if len(t) != 1 || t[0] != '|' {
-				return fmt.Errorf("expected | got '%s'", string(t))
-			}
-		case iOutput >= 0:
-			e.outputs[iOutput] = t
-			opts, ok := lenOpts[len(t)]
-			if !ok {
-				return fmt.Errorf("unexpected length for pattern: '%s'", string(t))
-			}
-			lib.Dbg("  out: '%s' opts: %#v\n", string(t), opts)
-			e.outOpts[string(t)] = opts
-		}
-	}
-	return nil
-}
+type entry [4]string
 
 type notes struct {
-	es []*entry
+	es []entry
 }
-
-func (n *notes) Part1() float64 {
-	r := 0.0
-	for _, e := range n.es {
-		for _, p := range e.outputs {
-			if len(lenOpts[len(p)]) == 1 {
-				r++
-			}
-		}
-	}
-	return r
-}
-func (n *notes) Part2() float64 { return 0.0 }
 
 func New(scanner *bufio.Scanner) (lib.Solution, error) {
 	r := &notes{}
@@ -86,7 +24,198 @@ func New(scanner *bufio.Scanner) (lib.Solution, error) {
 		if err != nil {
 			return nil, err
 		}
-		r.es = append(r.es, e)
+		r.es = append(r.es, *e)
 	}
 	return r, nil
+}
+
+func (e *entry) Scan(s fmt.ScanState, verb rune) error {
+	outputs := [4]string{}
+	signals := [10]string{}
+	for i := 0; i < len(signals)+1+len(outputs); i++ {
+		t, err := s.Token(true, nil)
+		if err != nil {
+			return err
+		}
+
+		iOutput := i - len(signals) - 1
+		iSep := len(signals)
+
+		switch {
+		case i < len(signals):
+			signals[i] = string(t)
+		case i == iSep:
+			if len(t) != 1 || t[0] != '|' {
+				return fmt.Errorf("expected | got '%s'", string(t))
+			}
+			continue
+		case iOutput >= 0:
+			outputs[iOutput] = string(t)
+		}
+	}
+
+	opts := newSegOpts()
+	if err := opts.Analyze(signals); err != nil {
+		return err
+	}
+
+	for i, from := range outputs {
+		to := make([]byte, len(from))
+		for j, b := range []byte(from) {
+			to[j] = opts.lookup(b)
+		}
+		sort.Slice(to, func(i, j int) bool { return to[i] < to[j] })
+		(*e)[i] = string(to)
+	}
+	return nil
+}
+
+type segSet map[byte]bool
+
+func fromString(s string) segSet {
+	out := make(map[byte]bool)
+	for _, b := range []byte(s) {
+		out[b] = true
+	}
+	return out
+}
+
+func allSegs() segSet {
+	return fromString("abcdefg")
+}
+
+func (s segSet) String() string {
+	r := make([]byte, 0, len(s))
+	for k, _ := range s {
+		r = append(r, k)
+	}
+	return string(r)
+}
+
+func (a segSet) intersect(b segSet) {
+	for k, v := range a {
+		if v != b[k] {
+			delete(a, k)
+		}
+	}
+}
+
+func (a segSet) union(b segSet) {
+	for k, _ := range b {
+		a[k] = true
+	}
+}
+
+func (a segSet) sub(b segSet) {
+	for k, _ := range b {
+		delete(a, k)
+	}
+}
+
+var (
+	canon = map[string]float64{
+		"abcefg": 0, "cf": 1, "acdeg": 2, "acdfg": 3, "bcdf": 4,
+		"abdfg": 5, "abdefg": 6, "acf": 7, "abcdefg": 8, "abcdfg": 9,
+	}
+
+	byLen      = map[int]segSet{}
+	byByteFreq = map[int]segSet{}
+)
+
+func init() {
+	byteFreqs := map[byte]int{}
+	for s, _ := range canon {
+		if byLen[len(s)] == nil {
+			byLen[len(s)] = segSet{}
+		}
+		byLen[len(s)].union(fromString(s))
+		for _, b := range []byte(s) {
+			byteFreqs[b]++
+		}
+	}
+	for k, v := range byteFreqs {
+		if byByteFreq[v] == nil {
+			byByteFreq[v] = segSet{}
+		}
+		byByteFreq[v][k] = true
+	}
+}
+
+type segOpts map[byte]segSet
+
+func newSegOpts() segOpts {
+	r := segOpts{}
+	for k, _ := range allSegs() {
+		r[k] = allSegs()
+	}
+	return r
+}
+
+func (s segOpts) String() string {
+	parts := make([]string, 0, len(s))
+	for k, v := range s {
+		parts = append(parts, fmt.Sprintf("%c: %s", k, &v))
+	}
+	return strings.Join(parts, ", ")
+}
+
+func (o segOpts) Analyze(signals [10]string) error {
+	byteFreqs := map[byte]int{}
+	for _, s := range signals {
+		set, ok := byLen[len(s)]
+		if !ok {
+			return fmt.Errorf("invalid length signal: %s", s)
+		}
+		for _, b := range []byte(s) {
+			o[b].intersect(set)
+			byteFreqs[b]++
+		}
+	}
+	for b, f := range byteFreqs {
+		o[b].intersect(byByteFreq[f])
+	}
+	mapped := segSet{}
+	for _, s := range o {
+		if len(s) == 1 {
+			mapped.union(s)
+		}
+	}
+	for _, s := range o {
+		if len(s) > 1 {
+			s.sub(mapped)
+		}
+		if len(s) != 1 {
+			return fmt.Errorf("unable to determine mapping from signals: %v (opts: %s)", signals, &o)
+		}
+	}
+	return nil
+}
+
+func (o segOpts) lookup(seg byte) byte {
+	return o[seg].String()[0]
+}
+
+func (n *notes) Part1() float64 {
+	r := 0.0
+	for _, e := range n.es {
+		for _, o := range e {
+			if len(o) == 7 || len(byLen[len(o)]) < 7 {
+				r++
+			}
+		}
+	}
+	return r
+}
+
+func (n *notes) Part2() float64 {
+	r := 0.0
+	for _, e := range n.es {
+		n := 0.0
+		for i, o := range e {
+			n += canon[o] * math.Pow(10, float64(len(e)-i-1))
+		}
+		lib.Dbg("%s: %f\n", strings.Join(e[:], " "), n)
+		r += n
+	}
+	return r
 }
--- adventofcode2021/day08/day08_test.go
+++ adventofcode2021/day08/day08_test.go
@@ -10,6 +10,8 @@ func TestPart1(t *testing.T) {
 	testCases := []*lib.TestCase{
 		{FName: "./sample.txt", Part1: true, Want: 26},
 		{FName: "./input.txt", Part1: true, Want: 525},
+		{FName: "./sample.txt", Part1: false, Want: 61229},
+		{FName: "./input.txt", Part1: false, Want: 1083859},
 	}
 	lib.Test(t, testCases, lib.Convert(New))
 }
