9ebfce72014949c410c8e2906e96cc1d9b625d4b
Alan Strohm <3605394+alan-strohm@users.noreply.github.com>
2021-12-17 09:40:46 -0800

    adventofcode2021: day16 part2

--- adventofcode2021/day16/day16.go
+++ adventofcode2021/day16/day16.go
@@ -10,77 +10,176 @@ import (
 type pktType uint8
 
 const (
-	literalPkt pktType = 4
+	sumPkt pktType = iota
+	productPkt
+	minPkt
+	maxPkt
+	literalPkt
+	gtPkt
+	ltPkt
+	eqPkt
 )
 
+func (p pktType) numArgs() int {
+	switch {
+	case p < literalPkt:
+		return -1
+	case p > literalPkt:
+		return 2
+	default:
+		return 0
+	}
+}
+
 type pkt struct {
-	ver  uint8
-	typ  pktType
-	lit  int
-	args []*pkt
+	ver   uint8
+	typ   pktType
+	lit   uint64
+	args  []*pkt
+	depth int
+}
+
+func (p *pkt) eval() (r uint64) {
+	if p.typ.numArgs() != -1 && p.typ.numArgs() != len(p.args) {
+		panic("invalid packet")
+	}
+	switch p.typ {
+	case sumPkt:
+		for _, arg := range p.args {
+			r += arg.eval()
+		}
+	case productPkt:
+		r = 1
+		for _, arg := range p.args {
+			r *= arg.eval()
+		}
+	case minPkt:
+		r = p.args[0].eval()
+		for _, arg := range p.args[1:] {
+			o := arg.eval()
+			if o < r {
+				r = o
+			}
+		}
+	case maxPkt:
+		r = p.args[0].eval()
+		for _, arg := range p.args[1:] {
+			o := arg.eval()
+			if o > r {
+				r = o
+			}
+		}
+	case literalPkt:
+		r = p.lit
+	case ltPkt:
+		if p.args[0].eval() < p.args[1].eval() {
+			r = 1
+		}
+	case gtPkt:
+		if p.args[0].eval() > p.args[1].eval() {
+			r = 1
+		}
+	case eqPkt:
+		if p.args[0].eval() == p.args[1].eval() {
+			r = 1
+		}
+	default:
+		panic("unknown packet type")
+	}
+	return
 }
 
 type dec struct {
-	buf []byte
-	pos int
-	bit uint8
+	buf   []byte
+	pos   int
+	bit   uint8
+	depth int
 }
 
+const byteLen = 8
+const maxByte = uint8(255)
+
+// Read bits [from, to) from in, numbered left to right.
 func readBits(in byte, from, to uint8) uint8 {
-	mask := uint8(255) >> (8 - (to - from))
-	align := ((in << from) >> (from + (8 - to)))
+	mask := maxByte >> (byteLen - (to - from))
+	align := in >> (byteLen - to)
 	return align & mask
 }
 
+// Read the next n bits.
 func (d *dec) readBits(n uint8) uint64 {
-	lib.Dbg("reading %d bits from %d, %d\n", n, d.pos, d.bit)
 	var r uint64
-	if d.bit+n > 8 {
-		r = uint64(readBits(d.buf[d.pos], d.bit, 8))
-		n -= (8 - d.bit)
+	// Sync to byte boundary, if possible.
+	if d.bit+n > byteLen {
+		r = uint64(readBits(d.buf[d.pos], d.bit, byteLen))
+		n -= (byteLen - d.bit)
 		d.bit = 0
 		d.pos++
 	}
-	for ; n > 8; n -= 8 {
-		r <<= 8
+	// Read whole bytes, if possible.
+	for ; n > byteLen; n -= byteLen {
+		r <<= byteLen
 		r += uint64(d.buf[d.pos])
 		d.pos++
 	}
-	if d.bit+n <= 8 {
+	// Read excess bits, or bits within a byte.
+	if d.bit+n <= byteLen {
 		r <<= n
 		r += uint64(readBits(d.buf[d.pos], d.bit, d.bit+n))
 		d.bit += n
 	}
+	if d.bit == byteLen {
+		d.bit = 0
+		d.pos++
+	}
 	return r
 }
 
-func (d *dec) decodeString(in string) (*pkt, error) {
-	*d = dec{buf: make([]byte, 0, len(in)/2)}
-	if _, err := fmt.Sscanf(in, "%X", &d.buf); err != nil {
-		return nil, err
-	}
-	return d.nextPkt(), nil
+func (d *dec) posString() string {
+	return fmt.Sprintf("d%dp%db%d", d.depth, d.pos, d.bit)
 }
 
-func (d *dec) readLiteral() {
+func (d *dec) check(cond bool, format string, a ...interface{}) {
+	if !cond {
+		panic(fmt.Sprintf(d.posString()+format, a...))
+	}
+}
+
+func (d *dec) dbg(format string, a ...interface{}) {
+	lib.Dbg(d.posString()+": "+format+"\n", a...)
+}
+
+func (d *dec) decodeString(in string) *pkt {
+	*d = dec{buf: make([]byte, 0, len(in)/2)}
+	if _, err := fmt.Sscanf(in, "%X", &d.buf); err != nil {
+		panic(err)
+	}
+	return d.nextPkt()
+}
+
+func (d *dec) readLiteral() (r uint64) {
 	for {
 		more := d.readBits(1)
-		d.readBits(4)
+		r <<= 4
+		r += d.readBits(4)
 		if more != 1 {
 			break
 		}
 	}
+	return
 }
 
 func (d *dec) readType0Args() []*pkt {
 	r := []*pkt{}
 	toRead := int(d.readBits(15))
+	d.dbg("reading %d, %d of packets", toRead/8, toRead%8)
 	end := d.pos + (int(d.bit)+toRead)/8
 	endBit := uint8((int(d.bit) + toRead) % 8)
-	lib.Dbg("stopping at byte %d, bit %d\n", end, endBit)
-	for d.pos < end || d.pos == end && d.bit < endBit {
+	for d.pos < end || (d.pos == end && d.bit < endBit) {
+		d.dbg("next packet")
 		r = append(r, d.nextPkt())
 	}
+	d.check(d.bit == endBit, "packet ended early (got %d, %d; want %d, %d)", d.pos, d.bit, end, endBit)
 	return r
 }
 
@@ -94,18 +193,22 @@ func (d *dec) readType1Args() []*pkt {
 }
 
 func (d *dec) nextPkt() *pkt {
-	r := &pkt{}
+	r := &pkt{depth: d.depth}
 	r.ver, r.typ = uint8(d.readBits(3)), pktType(d.readBits(3))
+	d.check(r.typ <= eqPkt, "unknown packet type %d", r.typ)
+	d.dbg("got %#v packet", r.typ)
 	if r.typ == literalPkt {
-		d.readLiteral()
+		r.lit = d.readLiteral()
 		return r
 	}
+	d.depth++
 	lengthType := d.readBits(1)
 	if lengthType == 1 {
 		r.args = d.readType1Args()
 	} else {
 		r.args = d.readType0Args()
 	}
+	d.depth--
 	return r
 }
 
@@ -122,10 +225,12 @@ func (p *pkt) Part1() int {
 	return sum
 }
 
-func (p *pkt) Part2() int { return 0 }
+func (p *pkt) Part2() int {
+	return int(p.eval())
+}
 
 func New(scanner *bufio.Scanner) (lib.Solution, error) {
 	scanner.Scan()
 	d := &dec{}
-	return d.decodeString(scanner.Text())
+	return d.decodeString(scanner.Text()), nil
 }
--- adventofcode2021/day16/day16_test.go
+++ adventofcode2021/day16/day16_test.go
@@ -11,22 +11,45 @@ func TestReadBits(t *testing.T) {
 		in          []byte
 		start, read uint8
 		want        uint64
+		wantBit     uint8
 	}{
-		{in: []byte{0xaa}, start: 0, read: 3, want: 5},
-		{in: []byte{0xaa}, start: 5, read: 3, want: 2},
-		{in: []byte{0xa1, 0xa0}, start: 7, read: 2, want: 3},
-		{in: []byte{0xa1, 0xff, 0x00}, start: 7, read: 10, want: 0x3fe},
+		{in: []byte{0xaa}, start: 0, read: 3, want: 5, wantBit: 3},
+		{in: []byte{0xaa}, start: 5, read: 3, want: 2, wantBit: 0},
+		{in: []byte{0xa1, 0xa0}, start: 7, read: 2, want: 3, wantBit: 1},
+		{in: []byte{0xa1, 0xff, 0x00}, start: 7, read: 10, want: 0x3fe, wantBit: 1},
 	}
 	for _, tc := range testCases {
 		d := &dec{buf: tc.in, bit: tc.start}
 		got := d.readBits(tc.read)
-		if got != tc.want {
-			t.Errorf("(%b, %d).readBits(%d) = %d, want %d", tc.in, tc.start, tc.read, got, tc.want)
+		if got != tc.want || d.bit != tc.wantBit {
+			t.Errorf("(%b, %d).readBits(%d) = %d, bit %d, want %d, bit %d", tc.in, tc.start, tc.read, got, d.bit, tc.want, tc.wantBit)
 		}
 	}
 }
 
-func TestPart1(t *testing.T) {
+func TestEval(t *testing.T) {
+	testCases := []struct {
+		in   string
+		want uint64
+	}{
+		{"C200B40A82", 3},
+		{"04005AC33890", 54},
+		{"880086C3E88112", 7},
+		{"CE00C43D881120", 9},
+		{"D8005AC2A8F0", 1},
+		{"F600BC2D8F", 0},
+		{"9C005AC2F8F0", 0},
+		{"9C0141080250320F1802104A08", 1},
+	}
+	for _, tc := range testCases {
+		d := &dec{}
+		if got := d.decodeString(tc.in).eval(); got != tc.want {
+			t.Errorf("%s.eval() = %d want %d", tc.in, got, tc.want)
+		}
+	}
+}
+
+func TestDay16(t *testing.T) {
 	testCases := []*lib.TestCase{
 		{FName: "./ex1.txt", Part1: true, Want: 6},
 		{FName: "./ex2.txt", Part1: true, Want: 16},
@@ -34,6 +57,7 @@ func TestPart1(t *testing.T) {
 		{FName: "./ex4.txt", Part1: true, Want: 23},
 		{FName: "./ex5.txt", Part1: true, Want: 31},
 		{FName: "./input.txt", Part1: true, Want: 971},
+		{FName: "./input.txt", Part1: false, Want: 831996589851},
 	}
 	lib.Test(t, testCases, lib.Convert(New))
 }
