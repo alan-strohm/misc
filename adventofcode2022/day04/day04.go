c706e76282bf76a7322ae0b6ad7e79fb21bdfeb8
Alan Strohm <3605394+alan-strohm@users.noreply.github.com>
2022-12-03 22:36:05 -0800

    adventofcode2022: day02 part2

--- adventofcode2022/day04/day04.go
+++ adventofcode2022/day04/day04.go
@@ -13,6 +13,12 @@ type interval struct {
 func (i interval) contains(o interval) bool {
 	return i.lo <= o.lo && i.hi >= o.hi
 }
+func (i interval) overlaps(o interval) bool {
+	if i.contains(o) || o.contains(i) {
+		return true
+	}
+	return (i.lo <= o.lo && i.hi >= o.lo) || (i.hi >= o.hi && i.lo <= o.hi)
+}
 
 func must(err error) {
 	if err != nil {
@@ -29,15 +35,18 @@ func parse(line string) (interval, interval) {
 }
 
 func Run(s *bufio.Scanner, part1 bool) (int, error) {
-	part1r := 0
+	part1r, part2r := 0, 0
 	for s.Scan() {
 		i1, i2 := parse(s.Text())
 		if i1.contains(i2) || i2.contains(i1) {
 			part1r++
 		}
+		if i1.overlaps(i2) {
+			part2r++
+		}
 	}
 	if part1 {
 		return part1r, nil
 	}
-	return 0, nil
+	return part2r, nil
 }
--- adventofcode2022/day04/day04_test.go
+++ adventofcode2022/day04/day04_test.go
@@ -9,7 +9,7 @@ import (
 func TestAll(t *testing.T) {
 	testCases := []*lib.TestCase{
 		{FName: "./input.txt", Part1: true, Want: 582},
-		{FName: "./input.txt", Part1: false, Want: 0},
+		{FName: "./input.txt", Part1: false, Want: 893},
 	}
 	lib.Test(t, testCases, Run)
 }
