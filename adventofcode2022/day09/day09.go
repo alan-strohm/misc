3b621b92e5a3c4f679d2f315777f7410e457e4d3
Alan Strohm <3605394+alan-strohm@users.noreply.github.com>
2022-12-10 17:16:26 -0800

    adventofcode2022: day09 part1

---
+++ adventofcode2022/day09/day09.go
@@ -0,0 +1,82 @@
+package day09
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+type pos [2]int
+
+func (p pos) sub(o pos) pos { return [2]int{p[0] - o[0], p[1] - o[1]} }
+func (p pos) add(o pos) pos { return [2]int{p[0] + o[0], p[1] + o[1]} }
+
+func newTail(h, t pos) pos {
+	diff := h.sub(t)
+	move := false
+	for i, dim := range diff {
+		abs := math.Abs(float64(dim))
+		if abs > 1 {
+			diff[i] = dim / int(abs)
+			move = true
+		}
+	}
+	if move {
+		return t.add(diff)
+	}
+	return t
+}
+
+type cmd struct {
+	dir   pos // unit vector
+	count int
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func parseCmd(l string) cmd {
+	var dir byte
+	var count int
+	_, err := fmt.Sscanf(l, "%c %d", &dir, &count)
+	must(err)
+
+	dirs := map[byte][2]int{
+		'R': [2]int{1, 0}, 'L': [2]int{-1, 0}, 'U': [2]int{0, 1}, 'D': [2]int{0, -1},
+	}
+	return cmd{dir: dirs[dir], count: count}
+}
+
+func parseCmds(s *bufio.Scanner) (r []cmd) {
+	for s.Scan() {
+		r = append(r, parseCmd(s.Text()))
+	}
+	return
+}
+
+func countVisited(cmds []cmd) int {
+	h, t := pos([2]int{0, 0}), pos([2]int{0, 0})
+	visited := map[[2]int]bool{t: true}
+	for _, c := range cmds {
+		for i := 0; i < c.count; i++ {
+			h = h.add(c.dir)
+			t = newTail(h, t)
+			visited[t] = true
+		}
+	}
+	return len(visited)
+}
+
+func Run(s *bufio.Scanner, isPart1 bool) (int, error) {
+	cmds := parseCmds(s)
+	if isPart1 {
+		return countVisited(cmds), nil
+		return 0, nil
+	}
+	return 0, nil
+}
---
+++ adventofcode2022/day09/day09_test.go
@@ -0,0 +1,36 @@
+package day09
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/alan-strohm/misc/adventofcode2022/lib"
+)
+
+func TestAll(t *testing.T) {
+	testCases := []*lib.TestCase{
+		{FName: "./input.txt", Part1: true, Want: 6486},
+		{FName: "./input.txt", Part1: false, Want: 0},
+	}
+	lib.Test(t, testCases, Run)
+}
+
+func TestNewTail(t *testing.T) {
+	testCases := []struct {
+		h, t, want pos
+	}{
+		{h: [2]int{0, 0}, t: [2]int{0, 0}, want: [2]int{0, 0}},
+		{h: [2]int{1, 0}, t: [2]int{0, 0}, want: [2]int{0, 0}},
+		{h: [2]int{1, 1}, t: [2]int{0, 0}, want: [2]int{0, 0}},
+		{h: [2]int{2, 0}, t: [2]int{0, 0}, want: [2]int{1, 0}},
+		{h: [2]int{2, 1}, t: [2]int{0, 0}, want: [2]int{1, 1}},
+		{h: [2]int{-2, -1}, t: [2]int{0, 0}, want: [2]int{-1, -1}},
+		{h: [2]int{2, 2}, t: [2]int{2, 4}, want: [2]int{2, 3}},
+	}
+	for _, tc := range testCases {
+		got := newTail(tc.h, tc.t)
+		if fmt.Sprint(got) != fmt.Sprint(tc.want) {
+			t.Errorf("newTail(%v, %v) = %v, want %v", tc.h, tc.t, got, tc.want)
+		}
+	}
+}
