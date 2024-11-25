c83b19e2736047ee4cb4ea301cc1deb47372736d
Alan Strohm <3605394+alan-strohm@users.noreply.github.com>
2024-11-24 22:29:28 -0800

    adventofcode2023: move nixpkgs to 24.05 and bump janet2nix

--- adventofcode2023/flake.nix
+++ adventofcode2023/flake.nix
@@ -3,7 +3,7 @@
     My solutions to the 2023 Advent of Code.
   '';
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.tar.gz";
     flake-utils.url = "github:numtide/flake-utils";
     janet2nix = {
       url = "git+ssh://git@github.com/alan-strohm/janet2nix";
